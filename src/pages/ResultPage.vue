1b65e4340e12fa8cd62e3efaaf41996fb52e4ef0
root_2 <yolotanvir@gmail.com>
2023-02-11 03:37:20 +0530

    Sample Quiz Page Fix
    Added padding to sampleQuizPage style.
    Other changes are code formatting with prettier.

--- src/pages/ResultPage.vue
+++ src/pages/ResultPage.vue
@@ -1,54 +1,63 @@
 <template>
- <div class="w-full h-full bg-blue-300">
-  <div class="grid place-items-center">
-    <div class="text-center">
-      <h1 class="font-bold text-white">
-        <span v-if="score >= Math.floor(resultArr.length / 2)">Yayy</span>
-        <span v-else>Awwn</span> 
-        you 
-        <span v-if="score >= Math.floor(resultArr.length / 2)">passed</span> 
-        <span v-else>didn't do too well on </span>
-        the quiz
-        <span v-if="score < Math.floor(resultArr.length / 2)">better luck next time</span>
-       </h1>
-      <h2 class="font-bold text-white">Your score was {{ score }}</h2>
-    </div>
-    <div class="flex flex-wrap justify-center px-2">
-      <div v-for="object in resultArr" :key="object" class="flex w-1/4 justify-center flex-col  font-semibold text-white">
-        <div>{{ object.question }}</div>
-        <div :class="object.selectedOption == object.answer ? 'text-green-600' :'text-red-500'">Your Answer : {{
-        object.selectedOption }}</div>
-        <div class="text-green-600">Correct Answer : {{ object.answer }}</div>
+  <div class="w-full h-full bg-blue-300">
+    <div class="grid place-items-center">
+      <div class="text-center">
+        <h1 class="font-bold text-white">
+          <span v-if="score >= Math.floor(resultArr.length / 2)">Yayy</span>
+          <span v-else>Awwn</span>
+          you
+          <span v-if="score >= Math.floor(resultArr.length / 2)">passed</span>
+          <span v-else>didn't do too well on </span>
+          the quiz
+          <span v-if="score < Math.floor(resultArr.length / 2)"
+            >better luck next time</span
+          >
+        </h1>
+        <h2 class="font-bold text-white">Your score was {{ score }}</h2>
+      </div>
+      <div class="flex flex-wrap justify-center px-2">
+        <div
+          v-for="object in resultArr"
+          :key="object"
+          class="flex w-1/4 justify-center flex-col font-semibold text-white"
+        >
+          <div>{{ object.question }}</div>
+          <div
+            :class="
+              object.selectedOption == object.answer
+                ? 'text-green-600'
+                : 'text-red-500'
+            "
+          >
+            Your Answer : {{ object.selectedOption }}
+          </div>
+          <div class="text-green-600">Correct Answer : {{ object.answer }}</div>
+        </div>
       </div>
     </div>
   </div>
- </div>
 </template>
 
 <script>
 export default {
-  mounted(){
+  mounted() {
     // console.log(JSON.parse(this.$route.params.resultArr))
   },
-  data(){
-  return {
-    score: this.$route.params.score,
-    resultArr: JSON.parse(this.$route.params.resultArr),
-    }
+  data() {
+    return {
+      score: this.$route.params.score,
+      resultArr: JSON.parse(this.$route.params.resultArr),
+    };
   },
-  methods : {
-    renderResultAnalysis(){
-
-    }
+  methods: {
+    renderResultAnalysis() {},
   },
-  mounted(){
-  }
-
-}
+  mounted() {},
+};
 </script>
 
 <style>
-.ls{
+.ls {
   background: rgba(22, 3, 4, 0.623);
 }
 </style>
